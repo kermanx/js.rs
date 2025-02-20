51cb49ea3803949f9f6dd2168feb604295ec668a
_Kerman <kermanx@qq.com>
2025-02-20 22:30:59 +0800

    feat: generateUseItem

--- packages/language-server/src/utils/index.ts
+++ packages/language-server/src/utils/index.ts
@@ -1,15 +1,36 @@
 import type { CodeInformation } from "@volar/language-core";
 import type { Code } from "../types";
 
-export function* wrapWith(
+export function wrapWith(
   start: number,
   end: number,
   features: CodeInformation,
   ...codes: Code[]
+): Generator<Code>;
+export function wrapWith(
+  start: number,
+  end: number,
+  features: CodeInformation,
+  codes: () => Iterable<Code>
+): Generator<Code>;
+export function* wrapWith(
+  start: number,
+  end: number,
+  features: CodeInformation,
+  ...codes: Code[] | [() => Iterable<Code>]
 ): Generator<Code> {
   yield [``, start, features];
   let offset = 1;
-  for (const code of codes) {
+
+  let normalized: Iterable<Code>;
+  if (codes.length === 1 && typeof codes[0] === "function") {
+    normalized = codes[0]();
+  }
+  else {
+    normalized = codes as Code[];
+  }
+
+  for (const code of normalized) {
     if (typeof code !== "string") {
       offset++;
     }
